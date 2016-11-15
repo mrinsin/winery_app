c4c8f928ccc8f421ca07bbd7b161d302a0121822
Mrinalini Sinha <ms6471@nyu.edu>
2016-11-15 10:26:08 -0800

    Small changes

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,3 +1,6 @@
 class Wine < ApplicationRecord
   belongs_to :winery
+  validates :name, uniqueness: true
+  validates :name, :vintage, :description, presence: true
+  validates :description, length: { in: 10..100 }
 end
